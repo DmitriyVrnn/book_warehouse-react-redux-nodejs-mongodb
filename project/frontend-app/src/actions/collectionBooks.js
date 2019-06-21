0f87cda7adb17ee52be5d473449af6ccb260a022
Dmitriy <kaishien777@gmail.com>
2019-06-21 16:57:48 +0300

    rewrote action on async await

--- project/frontend-app/src/actions/collectionBooks.js
+++ project/frontend-app/src/actions/collectionBooks.js
@@ -1,29 +1,36 @@
 import axios from 'axios';
-import { FETCH_COLLECTION_BOOK, DELETE_BOOK, API_URL } from '../constants/constants';
+import {
+  FETCH_COLLECTION_BOOK, DELETE_BOOK, API_URL,
+} from '../constants/constants';
 
-export const fetchCollectionBooks = books => ({
-  type: FETCH_COLLECTION_BOOK,
-  books,
-});
+export const fetchAllCollectionBooks = () => async (dispatch) => {
+  const fetchCollectionBooks = (books) => {
+    dispatch({
+      type: FETCH_COLLECTION_BOOK,
+      books,
+    });
+    return books;
+  };
+  try {
+    const books = await axios.get(`${API_URL}/book`);
+    return fetchCollectionBooks(books.data);
+  } catch (e) {
+    throw (e);
+  }
+};
 
-export const deleteBookSuccess = id => ({
-  type: DELETE_BOOK,
-  id,
-});
-
-export const deleteBook = id => dispatch => axios.get(`${API_URL}/book/delete/${id}`)
-  .then((res) => {
-    dispatch(deleteBookSuccess(res.data));
-  })
-  .catch((err) => {
-    throw (err);
-  });
-
-
-export const fetchAllCollectionBooks = () => dispatch => axios.get(`${API_URL}/book`)
-  .then((response) => {
-    dispatch(fetchCollectionBooks(response.data));
-  })
-  .catch((error) => {
-    throw (error);
-  });
+export const deleteBook = id => async (dispatch) => {
+  const deleteBookSuccess = (id) => {
+    dispatch({
+      type: DELETE_BOOK,
+      id,
+    });
+    return id;
+  };
+  try {
+    const book = await axios.get(`${API_URL}/book/delete/${id}`);
+    return deleteBookSuccess(book.data);
+  } catch (e) {
+    throw (e);
+  }
+};
