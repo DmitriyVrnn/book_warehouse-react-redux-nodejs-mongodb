30aa2878106fb4af9d5bcdef0dff409682441709
Dmitriy <kaishien777@gmail.com>
2019-06-21 16:29:45 +0300

    rewrote action on async await

--- project/frontend-app/src/actions/post.js
+++ project/frontend-app/src/actions/post.js
@@ -3,53 +3,53 @@ import {
   ADD_POST, DELETE_POST, FETCH_POST, API_URL,
 } from '../constants/constants';
 
-export const createPostSuccess = data => ({
-  type: ADD_POST,
-  payload: {
-    _id: data._id,
-    title: data.title,
-    author: data.author,
-    date: data.date,
-    body: data.body,
-  },
-});
-
 export const createPost = ({
   title, author, body, date,
-}) => dispatch => axios.post(`${API_URL}/posts/add`, {
-  title, author, body, date,
-})
-  .then((response) => {
-    dispatch(createPostSuccess(response.data));
-  })
-  .catch((error) => {
-    throw (error);
-  });
+}) => async (dispatch) => {
+  const createPostSuccess = (data) => {
+    const {
+      _id, title, author, date, body,
+    } = data;
+    dispatch({
+      type: ADD_POST,
+      payload: {
+        _id, title, author, date, body,
+      },
+    });
+    return data;
+  };
+  try {
+    const post = await axios.post(`${API_URL}/posts/add`, {
+      title, author, body, date,
+    });
+    return createPostSuccess(post.data);
+  } catch (e) {
+    throw (e);
+  }
+};
 
-export const deletePostSuccess = id => ({
-  type: DELETE_POST,
-  payload: {
-    id,
-  },
-});
+export const deletePost = id => async (dispatch) => {
+  const deletePostSuccess = (id) => {
+    dispatch({ type: DELETE_POST, payload: { id } });
+    return id;
+  };
+  try {
+    const post = await axios.get(`${API_URL}/posts/delete/${id}`);
+    return deletePostSuccess(post.data);
+  } catch (e) {
+    throw (e);
+  }
+};
 
-export const deletePost = id => dispatch => axios.get(`${API_URL}/posts/delete/${id}`)
-  .then((response) => {
-    dispatch(deletePostSuccess(response.data));
-  })
-  .catch((error) => {
-    throw (error);
-  });
-
-export const fetchPosts = posts => ({
-  type: FETCH_POST,
-  posts,
-});
-
-export const fetchAllPosts = () => dispatch => axios.get(`${API_URL}/posts`)
-  .then((response) => {
-    dispatch(fetchPosts(response.data));
-  })
-  .catch((error) => {
-    throw (error);
-  });
+export const fetchAllPosts = () => async (dispatch) => {
+  const fetchPosts = (posts) => {
+    dispatch({ type: FETCH_POST, posts });
+    return posts;
+  };
+  try {
+    const posts = await axios.get(`${API_URL}/posts`);
+    return fetchPosts(posts.data);
+  } catch (e) {
+    throw (e);
+  }
+};
